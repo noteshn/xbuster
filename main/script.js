00e132aa0300e01b896140035bf850ce39ff9943
Notesh <noteshndarks@gmail.com>
2025-11-22 23:45:47 +0600

    Add header comment to script.js
    Added header comment to describe script functionality and responsibilities.

--- main/script.js
+++ main/script.js
@@ -1,1 +1,12 @@
+/*
+  script.js
+  ------------------------------------------
+  This script runs in the popup page and
+  controls all user interactions.
 
+  Responsibilities:
+  - Connect UI controls (toggle, buttons, links)
+  - Read and write settings using chrome.storage
+  - Notify background.js when the user changes
+    extension preferences.
+*/
